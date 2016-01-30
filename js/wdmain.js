36110ad36451fadb88646bba013bcab045fc5764
Anass Al <mr@anassinator.com>
2016-01-29 19:30:21 -0500

    Clean up MtoR js

--- js/wdmain.js
+++ js/wdmain.js
@@ -1,10 +1,3 @@
-// var zombie = $('#zombie'),
-// 	pLocs = [0, -250, -500, -750, -1000, -1250, -1500],
-// 	curFrm = 0,
-// 	lastStep = 0,
-
-// 	animationCycle, backPosy;
-
 var robot = $('#Robot'),
 	pLocs = [0, -268, -536, -804, -1072, -1340, -1608, -1876, -2144],
 	curFrm = 0,
@@ -16,13 +9,8 @@ skrollr.init({
 			return;
 		}
 		if (o.curTop > lastStep + 2) {
-			// if(curFrm>=9){curFrm=0;}
 			robot.css('background-position', pLocs[curFrm++]+ 'px 0px');
 			lastStep = o.curTop;
-		// } else if (o.curTop < lastStep - 2){
-		// 	// if (curFrm<=-1){curFrm=9;}
-		// 	robot.css('background-position', pLocs[curFrm--] + 'px 0px');
-		// 	lastStep = o.curTop;
 		}
 	}
 });
