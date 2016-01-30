d31c60bb9d49ee56c82f0647fc44a5ea0618493c
Monica Ung <monica.ung@mail.mcgill.ca>
2016-01-30 17:03:27 -0500

    Debug header pinning

--- js/main.js
+++ js/main.js
@@ -1,10 +1,19 @@
+var doneTransformation = false;
+
 // Smooth scrolling
 $(function() {
   $('a[href*=#]:not([href=#])').click(function() {
     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
       var target = $(this.hash);
       target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-      if (target.length) {
+      if (target.length && doneTransformation == false) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 1700);
+        return false;
+      }
+      else
+      {
         $('html,body').animate({
           scrollTop: target.offset().top
         }, 600);
@@ -22,4 +31,42 @@ svg.setAttribute('height',  $(window).height() + 'px');
 // Stop judging me
 $( window ).resize(function() {
 svg.setAttribute('width',  $(window).width() + 'px');
+});
+
+// Sticky header
+$(function() {
+  // init ScrollMagic Controller
+  var controller = new ScrollMagic.Controller();
+
+  // Scene Handler
+  var scene = new ScrollMagic.Scene({
+    triggerElement: "#header", // point of execution
+    duration: 1200, // pin element for this duration
+    triggerHook: 0, // don't trigger until #pinned-trigger1 hits the top of the viewport
+    reverse: false // allows the effect to trigger when scrolled in the reverse direction
+  })
+  .setPin("#header"); // the element we want to pin
+  // Add Scenes to ScrollMagic Controller
+  controller.addScene([
+    scene,
+  ]);
+
+  // Transition
+  var robot = $('#Robot'),
+  	pLocs = [0, -268, -536, -804, -1072, -1340, -1608, -1876, -2144],
+  	curFrm = 0,
+  	lastStep = 0;
+
+  skrollr.init({
+  	beforerender: function(o){
+  		if (curFrm >= 9) {
+        doneTransformation = true;
+  			return;
+  		}
+  		if (o.curTop > lastStep + 50) {
+  			robot.css('background-position', pLocs[curFrm++]+ 'px 0px');
+  			lastStep = o.curTop;
+  		}
+  	}
+  });
 });
